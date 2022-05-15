23590bab82707af3b23742cebf0b757c55cd926b
barbozacecilia <barboza.ceciliam@gmail.com>
2022-05-15 17:17:40 -0300

    chore(Navigation): Setup basic navigation configuration

---
+++ src/routes/public/PublicRoutes.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+// This site has 3 pages, all of which are rendered
+// dynamically in the browser (not server rendered).
+//
+// Although the page does not ever refresh, notice how
+// React Router keeps the URL up to date as you navigate
+// through the site. This preserves the browser history,
+// making sure things like the back button and bookmarks
+// work properly.
+
+export default function PublicRoutes() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/sign-in">Sign-in</Link>
+          </li>
+          <li>
+            <Link to="/sign-up">Sign-up</Link>
+          </li>
+        </ul>
+        <hr />
+
+        <Routes>
+          <Route exact path="/">
+            {/*<Home/>*/}
+          </Route>
+          <Route path="/sign-in">{/*<SignUp />*/}</Route>
+          <Route path="/sign-up">{/* <SignIn/>*/}</Route>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+//            <Route path='/' element={<Home/>}/>
