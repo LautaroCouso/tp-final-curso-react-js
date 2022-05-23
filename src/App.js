2890cd6ddfb98ca356fd9b7e738761b2b1f1d169
LautaroCouso <lautaro.couso@gmail.com>
2022-05-23 20:23:28 -0300

    feat(Footer): added footer to all pages

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import PublicRoutes from './routes/public/PublicRoutes';
 import React from 'react';
 import './globalStyles/globalStyles.css';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <PublicRoutes />
+      <Footer />
     </div>
   );
 }
