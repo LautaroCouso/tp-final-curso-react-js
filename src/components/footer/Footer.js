3d63df89f11b6baca9484109c076664769d6d48a
LautaroCouso <lautaro.couso@gmail.com>
2022-05-24 17:27:11 -0300

    fix(Footer): made several changes to the footer component

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import { Image } from 'react-bootstrap';
+import inIcon from '../../assets/images/linkedinIcon.svg';
+import wappIcon from '../../assets/images/wappIcon.svg';
+import faceIcon from '../../assets/images/faceIcon.svg';
+import twitIcon from '../../assets/images/twitIcon.svg';
+import instIcon from '../../assets/images/instIcon.svg';
+import gooIcon from '../../assets/images/gooIcon.svg';
 
 const Footer = () => {
   return (
@@ -6,50 +13,24 @@ const Footer = () => {
       <div className="container p-4">
         <section className="mb-4">
           <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button">
-            <i className="fab fa-facebook-f" />
+            <Image src={faceIcon} />
           </a>
           <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button">
-            <i className="fab fa-twitter" />
+            <Image src={twitIcon} />
           </a>
           <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button">
-            <i className="fab fa-google" />
+            <Image src={gooIcon} />
           </a>
           <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button">
-            <i className="fab fa-instagram" />
+            <Image src={instIcon} />
           </a>
           <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button">
-            <i className="fab fa-linkedin-in" />
+            <Image src={inIcon} />
           </a>
           <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button">
-            <i className="fab fa-github" />
+            <Image src={wappIcon} />
           </a>
         </section>
-
-        <section className="">
-          <form action="">
-            <div className="row d-flex justify-content-center">
-              <div className="col-auto">
-                <p className="pt-2">
-                  <strong>Sign up for our newsletter</strong>
-                </p>
-              </div>
-              <div className="col-md-5 col-12">
-                <div className="form-outline form-white mb-4">
-                  <input type="email" id="form5Example21" className="form-control" />
-                  <label className="form-label" htmlFor="form5Example21">
-                    Email address
-                  </label>
-                </div>
-              </div>
-              <div className="col-auto">
-                <button type="submit" className="btn btn-outline-light mb-4">
-                  Subscribe
-                </button>
-              </div>
-            </div>
-          </form>
-        </section>
-
         <section className="mb-4">
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt distinctio earum repellat
@@ -60,109 +41,33 @@ const Footer = () => {
 
         <section className="">
           <div className="row">
-            <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
+            <div>
               <h5 className="text-uppercase">Links</h5>
 
-              <ul className="list-unstyled mb-0">
-                <li>
-                  <a href="#!" className="text-white">
-                    Link 1
+              <ul className="list-inline">
+                <li className="list-inline-item">
+                  <a href="/home" className="text-white">
+                    Home
                   </a>
                 </li>
-                <li>
-                  <a href="#!" className="text-white">
-                    Link 2
+                <li className="list-inline-item">
+                  <a href="/sign-up" className="text-white">
+                    Registro
                   </a>
                 </li>
-                <li>
-                  <a href="#!" className="text-white">
-                    Link 3
+                <li className="list-inline-item">
+                  <a href="/sign-in" className="text-white">
+                    Login
                   </a>
                 </li>
-                <li>
-                  <a href="#!" className="text-white">
-                    Link 4
+                <li className="list-inline-item">
+                  <a href="/new/modify/:id" className="text-white">
+                    Novedades
                   </a>
                 </li>
-              </ul>
-            </div>
-
-            <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-              <h5 className="text-uppercase">Links</h5>
-
-              <ul className="list-unstyled mb-0">
-                <li>
-                  <a href="#!" className="text-white">
-                    Link 1
-                  </a>
-                </li>
-                <li>
-                  <a href="#!" className="text-white">
-                    Link 2
-                  </a>
-                </li>
-                <li>
-                  <a href="#!" className="text-white">
-                    Link 3
-                  </a>
-                </li>
-                <li>
-                  <a href="#!" className="text-white">
-                    Link 4
-                  </a>
-                </li>
-              </ul>
-            </div>
-
-            <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-              <h5 className="text-uppercase">Links</h5>
-
-              <ul className="list-unstyled mb-0">
-                <li>
-                  <a href="#!" className="text-white">
-                    Link 1
-                  </a>
-                </li>
-                <li>
-                  <a href="#!" className="text-white">
-                    Link 2
-                  </a>
-                </li>
-                <li>
-                  <a href="#!" className="text-white">
-                    Link 3
-                  </a>
-                </li>
-                <li>
-                  <a href="#!" className="text-white">
-                    Link 4
-                  </a>
-                </li>
-              </ul>
-            </div>
-
-            <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-              <h5 className="text-uppercase">Links</h5>
-
-              <ul className="list-unstyled mb-0">
-                <li>
-                  <a href="#!" className="text-white">
-                    Link 1
-                  </a>
-                </li>
-                <li>
-                  <a href="#!" className="text-white">
-                    Link 2
-                  </a>
-                </li>
-                <li>
-                  <a href="#!" className="text-white">
-                    Link 3
-                  </a>
-                </li>
-                <li>
-                  <a href="#!" className="text-white">
-                    Link 4
+                <li className="list-inline-item">
+                  <a href="/add" className="text-white">
+                    Agregar Consola
                   </a>
                 </li>
               </ul>
